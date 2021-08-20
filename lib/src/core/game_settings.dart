45566600f9d2c2a2216fcf80c0697daea1e4475e
Obumuneme Nwabude <obumnwabude@gmail.com>
2021-08-20 04:08:46 +0100

    add more tests for board

--- lib/src/core/game_settings.dart
+++ lib/src/core/game_settings.dart
@@ -18,4 +18,16 @@ class GameSettings {
       includeJokers: true,
       useTwoDecks: false,
       enableUndoRedo: false);
+
+  GameSettings copyWith(
+      {int? initialHandSize,
+      bool? includeJokers,
+      bool? useTwoDecks,
+      bool? enableUndoRedo}) {
+    return GameSettings(
+        initialHandSize: initialHandSize ?? this.initialHandSize,
+        includeJokers: includeJokers ?? this.includeJokers,
+        useTwoDecks: useTwoDecks ?? this.useTwoDecks,
+        enableUndoRedo: enableUndoRedo ?? this.enableUndoRedo);
+  }
 }
--- test/board_test.dart
+++ test/board_test.dart
@@ -1,16 +1,52 @@
 import 'package:test/test.dart';
 import 'package:joker/core.dart';
-import 'package:joker/shell.dart';
 
 void main() {
-  final List<Player> players = [ShellPlayer(name: 'Test1')];
-  final GameSettings gS = GameSettings(initialHandSize: 10);
-  final Board board = Board(gameSettings: gS, players: players);
+  final List<Player> players = [SystemPlayer(name: 'Test1')];
+  final Board board = Board(
+      gameSettings: GameSettings.defaults()
+          .copyWith(initialHandSize: 10, enableUndoRedo: true),
+      players: players);
 
-  test('The Board to start the game when initialized', () {
-    expect(board.discardPile.size, 1);
-    expect(board.previous, board.discardPile[0]);
-    expect(board.drawPile.size, greaterThan(1));
-    expect(players[0].hand.size, 10);
+  group('The Board', () {
+    test('should start the game when initialized', () {
+      expect(board.discardPile.size, 1);
+      expect(board.previous, board.discardPile[0]);
+      expect(board.drawPile.size, greaterThan(1));
+      expect(players[0].hand.size, 10);
+    });
+    test('can draw a card to a player', () {
+      int oldDrawPileSize = board.drawPile.size;
+      int oldPlayerHandSize = players[0].hand.size;
+      board.draw(players[0]);
+      expect(board.drawPile.size, oldDrawPileSize - 1);
+      expect(players[0].hand.size, oldPlayerHandSize + 1);
+    });
+    test('should throw exception if unmatched card is played', () {
+      Card unmatchedCard;
+      if ([1, 4, 5].any((suit) => suit == board.previous.suit))
+        unmatchedCard = Card(rank: 14, suit: 6);
+      else
+        unmatchedCard = Card(rank: 14, suit: 5);
+
+      try {
+        board.play(players[0], unmatchedCard);
+      } catch (e) {
+        expect(e, isA<UnmatchedCardException>());
+      }
+    });
+    test('can accept a playable card by player', () {
+      int oldDiscardPileSize = board.discardPile.size;
+      // hard to use a cloned card as one can't tell if the Test SystemPlayer
+      // will have a matching card when this test runs.
+      board.play(players[0], Card.clone(board.previous));
+      expect(board.discardPile.size, oldDiscardPileSize + 1);
+    });
+    test('can undo and redo', () {
+      expect(board.canUndo, true);
+      board.undo();
+      expect(board.canRedo, true);
+      board.redo();
+    });
   });
 }
